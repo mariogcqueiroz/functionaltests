91a4b5b77559f09fad7d0debcb9f825d3fcc9b87
phdcoder <mestrealessandros@gmail.com>
2024-03-04 08:19:11 -0300

    GET e POST em PHP

--- php/web/index.php
+++ php/web/index.php
@@ -1,2 +1,23 @@
 <?php
-echo "Hello World!";
+
+function app() {
+    $path = $_SERVER["REQUEST_URI"];
+    $method = $_SERVER["REQUEST_METHOD"];
+    $data = "";
+
+    if ($path == "/app") {
+        $data = "Hello World on php!";
+    } elseif ($path == "/app/feedback" && $method == "POST") {
+        $name = $_POST['name'];
+        $email = $_POST['asdsdd'];
+        $feedback = $_POST['feedback'];
+        $data = "$name $email $feedback";
+    }
+
+    header("Content-Type: text/plain");
+    header("Content-Length: " .strlen($data));
+
+    echo $data;
+}
+
+app();
