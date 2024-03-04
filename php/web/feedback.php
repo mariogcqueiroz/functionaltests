0007dda025ab3751878664479cf9eba3c7b8574c
phdcoder <mestrealessandros@gmail.com>
2024-03-04 10:20:01 -0300

    Bando de dados em php

--- php/web/feedback.php
+++ php/web/feedback.php
@@ -11,22 +11,23 @@
 <div class="container">
     <h1>Formulário Feedback</h1>
     <form action="/app/feedback" method="post" >
+        <p class="text-danger"><?=$erro?></p>
         <div class="form-group">
             <label>
                 Nome
-                <input type="text" class="form-control" name="name"  placeholder="Digite seu nome" >
+                <input type="text" class="form-control" name="name"  placeholder="Digite seu nome" value="<?=$name?>" >
             </label>
         </div>
         <div class="form-group">
             <label>
                 Email
-                <input type="text" class="form-control" name="email" placeholder="Digite seu email" >
+                <input type="text" class="form-control" name="email" placeholder="Digite seu email"  value="<?=$email?>">
             </label>
         </div>
         <div class="form-group">
             <label>
                 Feedback
-                <input type="text"  class="form-control" name="feedback"  placeholder="Digite seu feedback">
+                <input type="text"  class="form-control" name="feedback"  placeholder="Digite seu feedback" value="<?=$feedback?>">
             </label>
         </div>
         <input type="submit" value="Enviar" class="btn btn-primary" >
--- php/web/index.php
+++ php/web/index.php
@@ -9,18 +9,26 @@ function app() {
         $data = "Hello World on php!";
     } else {
         if ($path == "/app/feedback") {
+            $name = "";
+            $email = "";
+            $feedback = "";
+            $erro="";
             if ($method == "POST") {
                 $name = $_POST['name'];
                 $email = $_POST['email'];
                 $feedback = $_POST['feedback'];
-                $data = "$name $email $feedback";
+                if (strpos($email,"@")) {
+                    $data = "$name $email $feedback";
+                } else {
+                    $erro="Email deve conter @";
+                    include ('feedback.php');
+                }
             }
             else {
                 include ('feedback.php');
             }
         }
     }
-
     echo $data;
 }
 
