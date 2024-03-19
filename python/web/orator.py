b75d730aa4383f933f126f76b555ba4bb03d7244
phdcoder <mestrealessandros@gmail.com>
2024-03-19 13:34:40 -0300

    Orator com migrations

---
+++ python/web/orator.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'pgsql': {
+        'driver': 'pgsql',
+        'host': 'db',
+        'database': 'site',
+        'user': 'app',
+        'password': 'app2024',
+        'prefix': ''
+    }
+}
