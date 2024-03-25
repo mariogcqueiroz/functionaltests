fc6fa347a30b831d1f81e710ce55a8588c59463f
phdcoder <mestrealessandros@gmail.com>
2024-03-25 10:09:05 -0300

    Table in index with buttons

--- php/app/views/feedback/index.php
+++ php/app/views/feedback/index.php
@@ -20,16 +20,22 @@
         <th>Nome</th>
         <th>Email</th>
         <th>Feedback</th>
+        <th>Actions</th>
     </tr>
     </thead>
     <tbody>
-    <?php /** @var array $results */
-    foreach ($results as $registro): ?>
+    <?php foreach ($results as $registro): ?>
         <tr>
             <td><?= $registro->id?></td>
             <td><?= $registro->nome?></td>
             <td><?= $registro->email?></td>
-            <td><?= $registro->feedback?></td>
+            <td><?= htmlspecialchars($registro->feedback)?></td>
+            <td>
+                <!-- Botão de edição -->
+                <a href="/app/feedback/update?id=<?= $registro->id ?>" class="btn btn-primary">Editar</a>
+                <!-- Botão de exclusão (com confirmação) -->
+                <a href="/app/feddback/delete?id=<?= $registro->id ?>" class="btn btn-danger" onclick="return confirm('Tem certeza que deseja excluir este item?')">Excluir</a>
+            </td>
         </tr>
     <?php endforeach; ?>
     </tbody>
