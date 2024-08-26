f9ca829c30e05ac0a437dd61ab84a864eb291f32
phdcoder <mestrealessandros@gmail.com>
2024-08-26 10:14:03 -0300

    Teste add Feedback

--- django/docker-django/src/myapp/tests.py
+++ django/docker-django/src/myapp/tests.py
@@ -13,3 +13,16 @@ class FeedBackTestCase(TestCase):
         response=c.get("/admin/myapp/feedback/add/")
         #print(response)
         self.assertEqual(response.status_code,200)
+    def test_can_add_feedback_form(self):
+        c = self.client
+        self.assertTrue(c.login(username="superadmin",password="superadmin"))
+        response=c.post("/admin/myapp/feedback/add/", {
+            "nome" : "teste",
+            "email" :"teste@gmail.com",
+            "feedback": "Nada a delarar",
+            "_save" : "Save"
+            }
+        )
+        self.assertEqual(response.status_code,302)
+        query= Feedback.objects.filter(nome="teste",email="teste@gmail.com",feedback="Nada a delarar")
+        self.assertEqual(query.count(),1)
