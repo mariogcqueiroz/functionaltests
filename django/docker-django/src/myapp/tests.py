13005e011c33a69886479f87f7c9bbddbfaafa1f
phdcoder <mestrealessandros@gmail.com>
2024-08-26 09:41:18 -0300

    FeedBackTestCase adicionado

--- django/docker-django/src/myapp/tests.py
+++ django/docker-django/src/myapp/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Feedback
 # Create your tests here.
+class FeedBackTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        us=User.objects.create_superuser("superadmin",password="superadmin")
+    def test_can_view_feedback_form(self):
+        c = self.client
+        self.assertTrue(c.login(username="superadmin",password="superadmin"))
+        response=c.get("/admin/myapp/feedback/add/")
+        #print(response)
+        self.assertEqual(response.status_code,200)
