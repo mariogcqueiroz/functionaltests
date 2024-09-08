06f154c027d6319f755f68a678b01238e2b8450f
mariogcqueiroz <mariogcqueiroz@gmail.com>
2024-09-08 10:09:33 -0300

    functionaltest 08/09 email

--- php/app/tests/functional/FeedbackCest.php
+++ php/app/tests/functional/FeedbackCest.php
@@ -12,19 +12,42 @@ class LoginFormCest
     {
         $I->amOnRoute('feedback/create');
         $I->submitForm('form', [
-            'Feedback[nome]' => 'Alessandro',
-            'Feedback[email]' => 'asdsd@asdasd.com',
-            'Feedback[idade]' => '23',
-            'Feedback[feedback]' => 'Nda a delcarar',
+            'Feedback[nome]' => 'Mario',
+            'Feedback[email]' => 'aspoi@auiop.com',
+            'Feedback[idade]' => '45',
+            'Feedback[feedback]' => 'Nd',
         ]);
         $I->expectTo('Encontrar registro na base de Dados');
         $result=$I->grabRecord('\app\models\Feedback',[
-            'nome' => 'Alessandro',
-            'email' => 'asdsd@asdasd.com',
-            'idade' => '23',
-            'feedback' => 'Nda a delcarar',
+            'nome' => 'Mario',
+            'email' => 'aspoi@auiop.com',
+            'idade' => '45',
+            'feedback' => 'Nd',
         ]);
         $I->assertNotEquals($result,null);
     }
+
+    public function sendInvalidEmail(\FunctionalTester $I)
+{
+    // Simulando o envio de um formulário de feedback com um email inválido
+    $I->amOnRoute('feedback/create');
+    $I->submitForm('form', [
+        'Feedback[nome]' => 'Mario',
+        'Feedback[email]' => 'email@invalidouiop98.com', // Email com domínio inválido (sem MX)
+        'Feedback[idade]' => '25',
+        'Feedback[feedback]' => 'Teste com email inválido',
+    ]);
+
+    $I->see("\"E-mail\" não é um endereço de e-mail válido.");
+    $I->expectTo('Não encontrar registro na base de Dados');
+    $result=$I->dontSeeRecord('\app\models\Feedback', [
+        'nome' => 'Mario',
+        'email' => 'email@invalidouiop98.com',
+        'idade' => '25',
+        'feedback' => 'Teste com email inválido',
+    ]);
+    $I->assertEquals($result,null);
+}
+
 
 }
